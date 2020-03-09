fc4a64c2594971398a2902ad5ac2414768538b9e
Enrique Ibarra <rique.ibarra1@gmail.com>
2020-03-09 14:40:00 -0500

    adding pokemon api assignment

---
+++ pokeapi/src/App.js
@@ -0,0 +1,32 @@
+import React, {useState} from 'react';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  const [pokemans, setPokemans] = useState([]);
+  const handleClick = () => {
+    fetch("https://pokeapi.co/api/v2/pokemon")
+          .then(response => {
+            return response.json();
+        }).then(response => {
+            console.log(response);
+            setPokemans(response.results)
+        }).catch(err=> {
+            console.log(err);
+        });
+  }
+  return (
+    <div className="App">
+      <button onClick={handleClick} className="btn btn-dark">Fetch Pokemans</button>
+      <ul>
+        {
+          pokemans.map((pokeman, idx) => {
+          return <li key={idx} index={idx}>{pokeman.name}</li>
+          })
+        }
+      </ul>
+    </div>
+  );
+}
+
+export default App;
